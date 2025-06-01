b69eb7dd0d3a1ddb3f41fd9604b397b9ba71fad8
lokeshkannang <lokeshbdms2003@gmail.com>
2025-06-01 16:43:25 +0530

    loki

--- fe/src/Component/Dashboard.tsx
+++ fe/src/Component/Dashboard.tsx
@@ -1,3 +1,6 @@
+//dashboard
+
+
 import React, { useEffect, useState } from "react";
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
@@ -6,14 +9,31 @@ import {
 import { getUserList } from "../API/API";
 
 const barData = [
-  { name: "Mon", sales: 120 },
-  { name: "Tue", sales: 180 },
-  { name: "Wed", sales: 90 },
-  { name: "Thu", sales: 200 },
-  { name: "Fri", sales: 140 },
-  { name: "Sat", sales: 170 },
-  { name: "Sun", sales: 240 },
+  { year: '2003', count: 500 },
+  { year: '2004', count: 2000 },
+  { year: '2005', count: 1500 },
+  { year: '2006', count: 5000 },
+  { year: '2007', count: 3500 },
+  { year: '2008', count: 9000 },
+  { year: '2009', count: 7000 },
+  { year: '2010', count: 15000 },
+  { year: '2011', count: 12000 },
+  { year: '2012', count: 25000 },
+  { year: '2013', count: 22000 },
+  { year: '2014', count: 40000 },
+  { year: '2015', count: 37000 },
+  { year: '2016', count: 55000 },
+  { year: '2017', count: 50000 },
+  { year: '2018', count: 70000 },
+  { year: '2019', count: 66000 },
+  { year: '2020', count: 85000 },
+  { year: '2021', count: 80000 },
+  { year: '2022', count: 95000 },
+  { year: '2023', count: 90000 },
+  { year: '2024', count: 100000 },
 ];
+
+
 
 const pieData = [
   { name: "Sarees", value: 400 },
@@ -26,124 +46,139 @@ const COLORS = ["#6366f1", "#10b981", "#f59e0b", "#ef4444"];
 
 
 const stats = [
-  { title: "Total Users", value: "1,250", icon: "👥",bgColor: "#0d6efd"  },
-  { title: "Products Sold (Today)", value: "75", icon: "🛍️",bgColor: "#0d6efd"  },
-  { title: "Products Sold (This Week)", value: "420", icon: "📦",bgColor: "#0d6efd"  },
-  { title: "Revenue (This Month)", value: "₹1.2L", icon: "💰" ,bgColor: "#0d6efd" },
+  { title: "Total Users", value: "1,250", icon: "👥", bgColor: "#0d6efd" },
+  { title: "Products Sold (Today)", value: "75", icon: "🛍️", bgColor: "#0d6efd" },
+  { title: "Products Sold (This Week)", value: "420", icon: "📦", bgColor: "#0d6efd" },
+  { title: "Revenue (This Month)", value: "₹1.2L", icon: "💰", bgColor: "#0d6efd" },
 ];
 
+
+
 export const Dashboard: React.FC = () => {
-  const [users,setUsers] = useState<any>([]);
-  const [barChat,setBarChat]=useState<any>([]);
-  const [pieChat,setPieChat]=useState<any>([]);
-  useEffect(()=>{
-   getUser();
-  },[])
-  const getUser = async()=>{
-    const response :any= await getUserList();
-    if(response?.status>200) setUsers(response?.data?.data);
+  const [users, setUsers] = useState<any>([]);
+  const [barChat, setBarChat] = useState<any>([]);
+  const [pieChat, setPieChat] = useState<any>([]);
+  useEffect(() => {
+    getUser();
+  }, [])
+  const getUser = async () => {
+    const response: any = await getUserList();
+    if (response?.status > 200) setUsers(response?.data?.data);
     console.log(response)
   }
   return (
     <div className="bg-gradient-to-br from-indigo-50 to-white min-h-screen p-8 space-y-8">
-    <div className="row mb-4">
-  {stats.map((stat, index) => (
-    <div
-      className="col-lg-4 mb-3"
-      key={index}
-    >
-      <div
-        className="d-flex p-3 rounded text-white dashboard-primary"
-        style={{minHeight:"120px" }} // fallback to indigo if no color
-      >
-        <div className="me-3 d-flex align-items-center fs-4">
-          {stat.icon}
-        </div>
-        <div>
-          <h4 className="text-sm text-light">{stat.title}</h4>
-          <p className="text-xl fw-bold text-white mt-1 mb-0">{stat.value}</p>
-        </div>
-      </div>
-    </div>
-  ))}
-</div>
-
-
-      {/* Charts Grid */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        {/* Bar Chart */}
-        <div className=" p-6 rounded-2xl">
-          <h2 className="text-clip-gradient mb-4">
-            Weekly Product Sales
-          </h2>
-          <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={barData}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Bar dataKey="sales" fill="#c98b8b" radius={[6, 6, 0, 0]} />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-
-        {/* Pie Chart */}
-        
+      {/* Stats Cards */}
+      <div className="row mb-4">
+        {stats.map((stat, index) => (
+          <div className="col-lg-3 mb-4" key={index}>
+            <div
+              className='cardContainer'     >
+              <div className='cardIcon'>{stat.icon}</div>
+              <div>
+                <h6 className='cardTitle'>{stat.title}</h6>
+                <p className='cardValue'>{stat.value}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
 
-      <div className="row mt-5 mb-5">
- 
-  <div className="col-lg-6 w-full rounded-2xl p-6">
-    <h2 className="text-clip-gradient mb-4">
-      Sales by Category
-    </h2>
+
+
+<div className="container mt-5 mb-5">
+  <div className="row g-4 align-items-stretch">
+    
+    {/* Left - Line Chart - chart-color */}
+  <div className="col-md-6">
+  <div className="rounded-3 p-3 bg-dark text-white h-100">
+    <h4 className="mb-4">
+      Year-wise Product Count
+    </h4>
     <ResponsiveContainer width="100%" height={300}>
-      <PieChart>
-        <Pie
-          data={pieData}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          outerRadius={100}
-          dataKey="value"
-          label={({ name, percent }) =>
-            `${name} ${(percent * 100).toFixed(0)}%`
-          }
-        >
-          {pieData.map((entry, index) => (
-            <Cell key={index} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-        <Tooltip />
-      </PieChart>
+      <LineChart data={barData}>
+        <CartesianGrid strokeDasharray="3 3" stroke="#ffffff33" />
+        <XAxis dataKey="year" stroke="#fff" />
+        <YAxis stroke="#fff" />
+        <Tooltip
+          contentStyle={{
+            backgroundColor: "#2d2d2d",
+            color: "#fff",
+            borderRadius: "10px"
+          }}
+        />
+        <Line
+          type="monotone"
+          dataKey="count"
+          stroke="#facc15"
+          strokeWidth={3}
+          dot={{ r: 5 }}
+        />
+      </LineChart>
     </ResponsiveContainer>
   </div>
+</div>
 
-  {/* Table Section */}
-  <div className="col-lg-6 w-full rounded-2xl p-6">
-    <h2 className="text-clip-gradient mb-4">
-      User Email List
-    </h2>
-    <div className="overflow-x-auto">
-      <table className="min-w-full table-auto border border-gray-200">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="px-4 py-2 border-b text-left text-sm text-gray-600">S.No</th>
-            <th className="px-4 py-2 border-b text-left text-sm text-gray-600">Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user:any, index:any) => (
-            <tr key={user.id || index} className="hover:bg-gray-50">
-              <td className="px-4 py-2 border-b text-sm text-gray-700">{index + 1}</td>
-              <td className="px-4 py-2 border-b text-sm text-gray-800">{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    {/* Right - Pie Chart */}
+    <div className="col-md-6">
+      <div className="rounded-3 p-3 bg-dark text-white h-100">
+        <h4 className="mb-4">
+          Sales by Category
+        </h4>
+        <ResponsiveContainer width="100%" height={300}>
+          <PieChart>
+            <Pie
+              data={pieData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              outerRadius={100}
+              dataKey="value"
+              label={({ name, percent }) =>
+                `${name} ${(percent * 100).toFixed(0)}%`
+              }
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={index} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </div>
+
   </div>
 </div>
+
+
+
+
+      {/* Table Section */}
+      {/* <div className="col-lg-6 w-full rounded-2xl p-6">
+          <h2 className="text-clip-gradient mb-4">
+            User Email List
+          </h2>
+          <div className="overflow-x-auto">
+            <table className="min-w-full table-auto border border-gray-200">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="px-4 py-2 border-b text-left text-sm text-gray-600">S.No</th>
+                  <th className="px-4 py-2 border-b text-left text-sm text-gray-600">Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user: any, index: any) => (
+                  <tr key={user.id || index} className="hover:bg-gray-50">
+                    <td className="px-4 py-2 border-b text-sm text-gray-700">{index + 1}</td>
+                    <td className="px-4 py-2 border-b text-sm text-gray-800">{user.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div> */}
+
 
     </div>
   );
