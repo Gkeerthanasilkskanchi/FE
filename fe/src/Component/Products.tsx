e790cdc0b7fc786b4a4bd2ecb10ba5ccb9abdcec
lokeshkannang <lokeshbdms2003@gmail.com>
2025-06-01 22:21:19 +0530

    loki

--- fe/src/Component/Products.tsx
+++ fe/src/Component/Products.tsx
@@ -8,7 +8,7 @@ export const Products = () => {
     const [products, setProducts] = useState<any>([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedProduct, setSelectedProduct] = useState<any | null>(null);
-    const [loading,setLoading]=useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetchProducts();
@@ -30,7 +30,7 @@ export const Products = () => {
         } catch (err: any) {
             console.error(err);
             toast.error(err.response?.data?.message);
-        }finally{
+        } finally {
             setLoading(false);
         }
     };
@@ -50,7 +50,7 @@ export const Products = () => {
         } catch (err: any) {
             console.error(err);
             toast.error(err.response?.data?.message);
-        }finally{
+        } finally {
             setLoading(false);
         }
     };
@@ -63,7 +63,7 @@ export const Products = () => {
             }
         } catch (error) {
             console.error("Failed to fetch products:", error);
-        }finally{
+        } finally {
             setLoading(false);
         }
     };
@@ -72,26 +72,26 @@ export const Products = () => {
             const email = sessionStorage.getItem("userEmail");
             if (!email) { toast.error("Login to buy a product"); }
             if (email) {
-            setLoading(true);
-            const payload = {
-                email,
-                id: product.id,
-                quantity: product.quantity || 1,
-                price: product.price,
-            };
-            await addOrderService(payload);
-             setLoading(false);
-            const message = encodeURIComponent(
-                `Hello, I'm interested in buying:\n\n` +
-                `🧵 *${product.title}*\n💰 Price: ₹${product.price}\n📦 Quantity: ${product.quantity || 1}\n\n` +
-                `Please provide further details.`
-            );
-            const whatsappNumber = "917904999697";
-            window.open(`https://wa.me/${whatsappNumber}?text=${message}`, '_blank');
-        }
+                setLoading(true);
+                const payload = {
+                    email,
+                    id: product.id,
+                    quantity: product.quantity || 1,
+                    price: product.price,
+                };
+                await addOrderService(payload);
+                setLoading(false);
+                const message = encodeURIComponent(
+                    `Hello, I'm interested in buying:\n\n` +
+                    `🧵 *${product.title}*\n💰 Price: ₹${product.price}\n📦 Quantity: ${product.quantity || 1}\n\n` +
+                    `Please provide further details.`
+                );
+                const whatsappNumber = "917904999697";
+                window.open(`https://wa.me/${whatsappNumber}?text=${message}`, '_blank');
+            }
         } catch (error) {
             console.error("Buy operation failed:", error);
-        }finally{
+        } finally {
             setLoading(false);
         }
     };
@@ -105,84 +105,88 @@ export const Products = () => {
 
     return (
         <>
-        <Loader loading={loading}></Loader>
-         <div className="container">
-            {/* Header & Filter */}
-            <div className="d-flex justify-content-between align-items-center mt-4">
-                <div className="w-100">
-                    <h4 className="text-center">Our Collections</h4>
+            <Loader loading={loading}></Loader>
+            <div className="container">
+                {/* Header & Filter */}
+                <div className="d-flex justify-content-between align-items-center mt-4">
+                    <div className="w-100">
+                        <h4 className="text-center">Our Collections</h4>
+                    </div>
+
                 </div>
 
-            </div>
+                {/* Product Grid */}
+                <div
+                    className="mt-4 overflow-auto"
+                    style={{ height: "calc(100vh - 80px)", width: "100%" }}
+                >
+                    <div className="row g-4">
+                        {products?.map((product: any) => (
+                            <div
+                                key={product.id}
+                                className="col-12 col-sm-6 col-lg-4"
 
-            {/* Product Grid */}
-            <div
-                className="mt-4 overflow-auto"
-                style={{ height: "calc(100vh - 80px)", width: "100%" }}
-            >
-                <div className="row g-4">
-                    {products?.map((product: any) => (
-                        <div
-                            key={product.id}
-                            className="col-12 col-sm-6 col-lg-4"
-                             
-                        >
-                            <div className="card h-100 shadow-sm border-0" >
-                                <img
-                                    src={product?.image || '/images/default.jpg'}
-                                    className="card-img-top"
-                                    style={{
-                                        height: '300px',
-                                        width: '100%',
-                                        objectFit: 'fill',
-                                        objectPosition: 'center',
-                                        backgroundColor: '#f8f8f8',
-                                        cursor: 'pointer'
-                                    }}
-                                   
-                                    alt={product.title}
-                                   
-                                    
-                                />
+                            >
+                                <div className="card h-100 shadow-sm border-0" >
+                                    <img
+                                        src={product?.image || '/images/default.jpg'}
+                                        className="card-img-top"
+                                        style={{
+                                            height: '300px',
+                                            width: '100%',
+                                            objectFit: 'fill',
+                                            objectPosition: 'center',
+                                            backgroundColor: '#f8f8f8',
+                                            cursor: 'pointer'
+                                        }}
 
-                                <div className="card-body d-flex flex-column justify-content-between">
-                                    <span
-                                        className="fw-bold d-block mb-2 text-truncate"
-                                        style={{ cursor: "pointer" ,textDecoration:'underline'}}
-                                        onClick={() => handleImageClick(product)}
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#productDetailModal"
-                                        title={product.title}
-                                    >
-                                        {product.title}
-                                    </span>
-                                    <div className="d-flex justify-content-between align-items-center gap-2 mt-auto ps-4 pe-4">
-                                        <button
-                                            style={{ outline: 'none', border: 'none', borderRadius: '5px' }}
-                                            className="primary"
-                                            title="Buy Now"
-                                            onClick={() => handleBuyClick(product)}
+                                        alt={product.title}
+
+
+                                    />
+
+                                    <div className="card-body d-flex flex-column justify-content-between">
+                                        <span
+                                            className="fw-bold d-block mb-2 text-truncate"
+                                            style={{ cursor: "pointer", textDecoration: 'underline' }}
+                                            onClick={() => handleImageClick(product)}
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#productDetailModal"
+                                            title={product.title}
                                         >
-                                            <i className="bi bi-bag-fill me-1" style={{ fontSize: "30px" }}></i>
-                                        </button>
+                                            {product.title}
+                                        </span>
+                                        <div className="d-flex justify-content-between align-items-center gap-2 mt-auto ps-4 pe-4">
+                                            <button
+                                                style={{ outline: 'none', border: 'none', borderRadius: '5px' }}
+                                                className="primary"
+                                                title="Buy Now"
+                                                onClick={() => handleBuyClick(product)}
+                                            >
+                                                <i className="bi bi-bag " style={{ fontSize: "30px" }}></i>
+                                            </button>
 
-                                        <button style={{ outline: 'none', border: 'none', borderRadius: '5px' }} className="primary" title="Add to Cart" onClick={() => addToCart(product.id)}>
-                                            <i className="bi bi-cart-plus-fill" style={{ fontSize: "30px" }}></i>
-                                        </button>
-                                        <button style={{ outline: 'none', border: 'none', borderRadius: '5px' }} className="primary" title="Like Product" onClick={() => likeProduct(product.id)}>
-                                            <i className="bi bi-hand-thumbs-up-fill" style={{ fontSize: "30px" }}></i>
-                                        </button>
+                                            <button style={{ outline: 'none', border: 'none', borderRadius: '5px' }} className="primary" title="Add to Cart" onClick={() => addToCart(product.id)}>
+                                                <i className="bi bi-cart" style={{ fontSize: "30px" }}></i>
+                                                <i className="bi bi-cart-plus" style={{ fontSize: "30px" }}></i>
+
+                                            </button>
+                                            <button style={{ outline: 'none', border: 'none', borderRadius: '5px' }} className="primary" title="Like Product" onClick={() => likeProduct(product.id)}>
+                                                <i className="bi bi-suit-heart text-danger" style={{ fontSize: '30px' }}></i>
+                                                <i className="bi bi-heart-fill text-danger" style={{ fontSize: '30px' }}></i>
+
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
 
 
 
-            {/* <div
+                {/* <div
                 className="modal fade"
                 id="imageModal"
                 tabIndex={-1}
@@ -265,50 +269,50 @@ export const Products = () => {
 
 
 
-            {/* PRODUCT DETAIL MODAL */}
-            <div className="modal fade" id="productDetailModal" tabIndex={-1} aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" style={{ maxWidth: "600px" }}>
-                    <div className="modal-content position-relative">
-                        {/* Product Info */}
-                        <div className="d-flex p-3" style={{ maxHeight: "300px" }}>
-                            {/* Left: Description */}
-                            <div className="w-70 pe-3" style={{ width: "70%" }}>
-                                <h5 className="text-clip-gradient">About the Product</h5>
-                                <p style={{ fontSize: "14px" }}>
-                                    {selectedProduct?.about}
-                                </p>
-                                <img
-                                    src={selectedProduct?.image || '/images/default.jpg'}
-                                    className="card-img-top"
-                                    style={{
-                                        height: '200px',
-                                        width: '100%',
-                                        objectFit: 'contain',
-                                        objectPosition: 'center',
-                                        backgroundColor: '#f8f8f8',
-                                        cursor: 'pointer'
-                                    }}
-                                    alt={selectedProduct?.title}
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#imageModal"
-                                />
+                {/* PRODUCT DETAIL MODAL */}
+                <div className="modal fade" id="productDetailModal" tabIndex={-1} aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" style={{ maxWidth: "600px" }}>
+                        <div className="modal-content position-relative">
+                            {/* Product Info */}
+                            <div className="d-flex p-3" style={{ maxHeight: "300px" }}>
+                                {/* Left: Description */}
+                                <div className="w-70 pe-3" style={{ width: "70%" }}>
+                                    <h5 className="text-clip-gradient">About the Product</h5>
+                                    <p style={{ fontSize: "14px" }}>
+                                        {selectedProduct?.about}
+                                    </p>
+                                    <img
+                                        src={selectedProduct?.image || '/images/default.jpg'}
+                                        className="card-img-top"
+                                        style={{
+                                            height: '200px',
+                                            width: '100%',
+                                            objectFit: 'contain',
+                                            objectPosition: 'center',
+                                            backgroundColor: '#f8f8f8',
+                                            cursor: 'pointer'
+                                        }}
+                                        alt={selectedProduct?.title}
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#imageModal"
+                                    />
 
+                                </div>
+
+                                {/* Right: Brief info */}
+                                <div className="w-30 border-start ps-3" style={{ width: "30%" }}>
+                                    <p><strong>Name:</strong> {selectedProduct?.title}</p>
+                                    <p><strong>Price:</strong> ₹{selectedProduct?.price}</p>
+                                    <p><strong>Cloth:</strong> {selectedProduct?.cloth || "N/A"}</p>
+                                    <p><strong>Category:</strong> {selectedProduct?.category || "Traditional"}</p>
+                                    {/* <p><strong>Bought By:</strong> {selectedProduct?.bought_by || "N/A"} customers</p> */}
+                                    <p><strong>Saree Type:</strong> {selectedProduct?.saree_type}</p>
+
+                                </div>
                             </div>
 
-                            {/* Right: Brief info */}
-                            <div className="w-30 border-start ps-3" style={{ width: "30%" }}>
-                                <p><strong>Name:</strong> {selectedProduct?.title}</p>
-                                <p><strong>Price:</strong> ₹{selectedProduct?.price}</p>
-                                <p><strong>Cloth:</strong> {selectedProduct?.cloth || "N/A"}</p>
-                                <p><strong>Category:</strong> {selectedProduct?.category || "Traditional"}</p>
-                                {/* <p><strong>Bought By:</strong> {selectedProduct?.bought_by || "N/A"} customers</p> */}
-                                <p><strong>Saree Type:</strong> {selectedProduct?.saree_type}</p>
 
-                            </div>
-                        </div>
-
-
-                        {/* <div className="px-3 pb-3">
+                            {/* <div className="px-3 pb-3">
                             <h6>Customer Reviews</h6>
                             <div id="reviewCarousel" className="carousel slide" data-bs-ride="carousel">
                                 <div className="carousel-inner">
@@ -363,27 +367,27 @@ export const Products = () => {
 
 
 
-                        <div className="border-top p-3 d-flex justify-content-between">
-                            <button
-                                style={{ outline: 'none', border: 'none', borderRadius: '5px' }}
-                                className="primary"
-                                title="Buy Now"
-                                onClick={() => handleBuyClick(selectedProduct)}
-                            >
-                                <i className="bi bi-bag-fill me-1" style={{ fontSize: "30px" }}></i>
-                            </button>
-                            <button className="btn btn-secondary me-2" onClick={() => addToCart(selectedProduct.id)}><i className="bi bi-cart-plus"></i></button>
-                            <button className="btn btn-outline-danger" onClick={() => likeProduct(selectedProduct.id)}><i className="bi bi-heart"></i></button>
-                        </div>
+                            <div className="border-top p-3 d-flex justify-content-between">
+                                <button
+                                    style={{ outline: 'none', border: 'none', borderRadius: '5px' }}
+                                    className="primary"
+                                    title="Buy Now"
+                                    onClick={() => handleBuyClick(selectedProduct)}
+                                >
+                                    <i className="bi bi-bag-fill me-1" style={{ fontSize: "30px" }}></i>
+                                </button>
+                                <button className="btn btn-secondary me-2" onClick={() => addToCart(selectedProduct.id)}><i className="bi bi-cart-plus"></i></button>
+                                <button className="btn btn-outline-danger" onClick={() => likeProduct(selectedProduct.id)}><i className="bi bi-heart"></i></button>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
+
+
+
             </div>
-
-
-
-        </div>
         </>
-       
+
     );
 };
