32fdb02e68944fe1933d5575ef4e3506248d8ac4
lokeshkannang <lokeshbdms2003@gmail.com>
2025-06-17 22:25:49 +0530

    loki

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { About } from "./Component/About";
 import { AdminDashboard } from "./Component/AdminDashboard";
@@ -8,25 +8,21 @@ import { Layout } from "./Component/Layout";
 import { ProductList } from "./Component/ProductList";
 import { Products } from "./Component/Products";
 
-
 function AppRouter() {
   return (
-    <BrowserRouter>
+    <>
       <ToastContainer />
       <Routes>
         <Route path="/" element={<Layout />}>
-         
-            <Route path="dashboard" element={<AdminDashboard />} />
-              <Route index element={<Products />} /> 
-              <Route path="home" element={<Home />} />
-              <Route path="contact-us" element={<Contact />} />
-              <Route path="about" element={<About />} />
-              <Route path="products/:type" element={<ProductList />} />
-          
-         
+          <Route index element={<Products />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="home" element={<Home />} />
+          <Route path="contact-us" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="products/:type" element={<ProductList />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
